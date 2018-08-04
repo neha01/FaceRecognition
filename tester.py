170eebd88658026d5be89faf6025064d878d6013
Neha Yadav <akashmac@Akashs-MacBook-Air.local>
2018-08-04 20:07:22 +0530

    First commit

---
+++ tester.py
@@ -0,0 +1,42 @@
+import cv2
+import os
+import numpy as np
+import faceRecognition as fr
+
+
+#This module takes images  stored in diskand performs face recognition
+test_img=cv2.imread('Tutorial_faceRecognition/TestImages/neha1.jpg')
+faces_detected,gray_img=fr.faceDetection(test_img)
+print("faces_detected:",faces_detected)
+
+
+#Uncomment belows lines when running this program first time.Since it svaes training.yml file in directory
+# faces,faceID=fr.labels_for_training_data('Tutorial_faceRecognition/trainingImages')
+# face_recognizer=fr.train_classifier(faces,faceID)
+# face_recognizer.save('trainingData.yml')
+face_recognizer=cv2.face.LBPHFaceRecognizer_create()
+face_recognizer.read('trainingData.yml')#use this to load training data for subsequent runs
+
+name={0:"Priyanka",1:"Neha"}#creating dictionary containing names for each label
+
+for face in faces_detected:
+    (x,y,w,h)=face
+    roi_gray=gray_img[y:y+h,x:x+h]
+    label,confidence=face_recognizer.predict(roi_gray)#predicting the label of given image
+    print("confidence:",confidence)
+    print("label:",label)
+    fr.draw_rect(test_img,face)
+    predicted_name=name[label]
+    if(confidence>37):#If confidence less than 37 then don't print predicted face text on screen
+        continue
+    fr.put_text(test_img,predicted_name,x,y)
+
+resized_img=cv2.resize(test_img,(1000,1000))
+cv2.imshow("face dtecetion tutorial",resized_img)
+cv2.waitKey(0)#Waits indefinitely until a key is pressed
+cv2.destroyAllWindows
+
+
+
+
+
